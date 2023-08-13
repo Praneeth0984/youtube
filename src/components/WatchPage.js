7c796f77fb6381ff80902e1860d2e421d0d0b485
Praneeth0984 <srinivasulukonduru16@gmail.com>
2023-08-13 13:39:31 +0530

    youtube app final  part

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,26 +2,35 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeMenu } from "../utils/appSlice";
+import CommentsContainer from "./CommentsContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get("v"));
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="px-5">
-      <iframe
-        width="1200"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col w-full">
+      <div className="px-5 flex w-full">
+        <div className="">
+          <iframe
+            width="1200"
+            height="600"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="w-full">
+          <LiveChat />
+        </div>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,5 +1,8 @@
 const GOOGLE_API_KEY = "AIzaSyBnzjxRbrpMtBOEJMAZY5Cv2bGElBhQ5yc";
+export const LIVE_CHAT_COUNT = 25;
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
+export const YOUTUBE_SEARCH_API =
+  "https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q=";
